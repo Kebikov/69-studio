5f5032f1825498383b2a7e1bf07d8e9ba16d073b
Kebikov Evgenii <kebikovgen@gmail.com>
2023-12-08 19:52:35 +0300

    added description in the function 'addedImgToProject'

--- js/modules/addedImgToProject.ts
+++ js/modules/addedImgToProject.ts
@@ -1,34 +1,61 @@
 /**
+ * Функция выподаюших фотографий при нажатии на титульную фотографию.
+ * - Фотографии будут добавлены после элемента с class="projects-our-story"
  * @example
  * <div 
         class="projects-our-story" 
         data-click="project" 
-        data-start="1" 
-        data-end="18" 
-        data-path="../img/portfolio/HIGH5 office building complex/"
+        data-start="{...номер начального изображения}" 
+        data-end="{...номер конечного изображения}" 
+        data-path="{...путь к папке с изображениями}"
     >
         <picture>
-            <source srcset="../img/portfolio/HIGH5 office building complex/4.webp" type="image/webp">
-            <img class="projects-our-story__title-img" src="../img/portfolio/HIGH5 office building complex/4.jpg" alt="my_alt">
+            <source srcset="{...путь к img.webp}" type="image/webp">
+            <img class="projects-our-story__title-img" src="{...путь к img.jpg}" alt="my_alt">
         </picture>
-        <div class="projects-our-story__title"><span>,,HIGH5" office building complex</span></div>
+        <div class="projects-our-story__title"><span>{...Имя проекта}</span></div>
     </div>
  */
 const addedImgToProject = () => {
     try {
+        /**
+         * Массив элементов с атрибутом ( data-click="project" ).
+         */
         const projectsElements = document.querySelectorAll('[data-click="project"]') as NodeListOf<HTMLInputElement>;
+        /**
+         * - Функция получает данные о начальном, конечном кадре и пути из дата атрибутов.
+         * - Добавляет изображения после радительского элемента.
+         * @param event Событие элемента на котором призошол клик.
+         * @returns 
+         */
         const addImg = (event: Event) => {
+            /**
+             * Дочерний элемент на котором произошел клик.
+             */
             const children = event.target as HTMLDivElement;
+            /**
+             * Родительский элемент, в котором находится дочерний элемент, на котором произошло событие.
+             */
             const parent = children.closest('[data-click="project"]') as HTMLDivElement;
 
             if(!parent) return;
-
+            /**
+             * Стартовый номер изображения для добавления.
+             * - В папке изображения пронумерованы цифрами от 1 и далее.
+             */
             const start: number = Number( parent.getAttribute('data-start') );
+            /**
+             * Конечный номер изображения для добавления.
+             * - В папке изображения пронумерованы цифрами от 1 и далее.
+             */
             const end: number = Number( parent.getAttribute('data-end') );
+            /**
+             * Путь к папке с изображениями.
+             */
             const path: string | null = parent.getAttribute('data-path');
-
+            // Если нет данных, то не добавляем изображения.
             if(!start && !end && !path) return;
-
+            // Добавление изображений.
             for(let i = end; i >= start; i--) {
                 parent.insertAdjacentHTML(
                     'afterend',
@@ -42,7 +69,9 @@ const addedImgToProject = () => {
             };
 
         }
+        // Проверка сушествования элементов projectsElements
         if(projectsElements) {
+            // Установка слушателей событий.
             projectsElements.forEach(project => {
                 project.addEventListener('click', addImg);
             });
